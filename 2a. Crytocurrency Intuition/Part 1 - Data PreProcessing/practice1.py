2068182f7aa39a066dabcbe0e29632fafab16a3d
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-01 01:41:00 +0600

    blockchail done

--- 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
+++ 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
@@ -1,7 +1,7 @@
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
 
 # Part 1 - Building a Blockchain
 
@@ -40,5 +40,24 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(
+                str(proof**2-previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+
+            return True
 # name = input("Name: ")
 # print("Hello", name)
