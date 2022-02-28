b142878b564e1cd9d2dd30864a02b8c76c423ce7
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-01 00:50:26 +0600

    program crypto v1

--- 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
+++ 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
@@ -1,16 +1,21 @@
-# import datetime
-# import hashlib
-# import json
-# from flask import Flask, jsonify
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
 
-# # Part 1 - Building a Blockchain
-
-# class Blockchain:
-
-#     def __init__(self):
-#         self.chain = []
-#         self.create_block(proof = 1, previous_hash = '0')
+# Part 1 - Building a Blockchain
 
 
-name = input("Name: ")
-print("Hello", name)
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now())}
+
+
+# name = input("Name: ")
+# print("Hello", name)
