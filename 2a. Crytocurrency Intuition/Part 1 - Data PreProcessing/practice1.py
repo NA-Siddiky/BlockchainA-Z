175fa99a971c9999100aadcf63071fff5d40e8fb
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-01 01:26:23 +0600

    program crypto v3

--- 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
+++ 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
@@ -16,10 +16,26 @@ class Blockchain:
         block = {
             'index': len(self.chain)+1,
             'timestamp': str(datetime.datetime.now()),
-            'proof': proof, 'previous_hash': previous_hash
+            'proof': proof,
+            'previous_hash': previous_hash
         }
         self.chain.append(block)
         return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(
+                str(new_proof**2-previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 12
+                return new_proof
 
 # name = input("Name: ")
 # print("Hello", name)
