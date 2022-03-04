cb5f4f211c806b6304f299f69a23184aa1818ba6
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-04 18:57:19 +0600

    test ok

--- 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
+++ 2a. Crytocurrency Intuition/Part 1 - Data PreProcessing/practice1.py
@@ -1,3 +1,6 @@
+# Module 1 - Create a Blockchain
+
+# Importing the libraries
 import datetime
 import hashlib
 import json
@@ -13,12 +16,10 @@ class Blockchain:
         self.create_block(proof=1, previous_hash='0')
 
     def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain)+1,
-            'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
-            'previous_hash': previous_hash
-        }
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
 
@@ -30,12 +31,12 @@ class Blockchain:
         check_proof = False
         while check_proof is False:
             hash_operation = hashlib.sha256(
-                str(new_proof**2-previous_proof**2).encode()).hexdigest()
+                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
-                new_proof += 12
-                return new_proof
+                new_proof += 1
+        return new_proof
 
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
@@ -48,20 +49,18 @@ class Blockchain:
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
-
             previous_proof = previous_block['proof']
             proof = block['proof']
             hash_operation = hashlib.sha256(
-                str(proof**2-previous_proof**2).encode()).hexdigest()
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
-
-            return True
-
+        return True
 
 # Part 2 - Mining our Blockchain
+
 
 # Creating a Web App
 app = Flask(__name__)
